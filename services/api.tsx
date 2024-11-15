ab6a806d7080bdf250e7e127cbfb229d0faed729
翁 <dennis1989999@gmail.com>
2024-11-15 21:07:38 +0800

    163.25.111.55:8000

--- services/api.tsx
+++ services/api.tsx
@@ -1,5 +1,5 @@
 export const fetchData = async () => {
-    const response = await fetch('http://localhost:8001/api');
+    const response = await fetch('http://163.25.111.55:8000/api');
     if (!response.ok) {
         throw new Error('獲取數據失敗');
         }
--- services/endpoint.ts
+++ services/endpoint.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8001'
+export const BASE_URL = 'http://163.25.111.55:8000'
 
 export const ENDPOINTS = {
     gen_video: `${BASE_URL}/storyboard/execute-generate-video`,
